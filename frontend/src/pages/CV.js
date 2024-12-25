46d2360174597592328ca448b5795b2f5b33d4b5
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-12-25 18:11:34 -0500

    Removed old CV component

--- frontend/src/pages/CV.js
+++ frontend/src/pages/CV.js
@@ -1,14 +1,5 @@
 // CV component
 import { toast } from 'react-toastify';
-import { pdfjs } from 'react-pdf';
-import { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-import cvDriveURL from '../content/cv.pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-	'pdfjs-dist/build/pdf.worker.min.mjs',
-	import.meta.url,
-).toString();
 
 function CV() {
 
@@ -63,48 +54,12 @@ function CV() {
 		}
 	};
 
-	// PDF viewer
-	const [numPages, setNumPages] = useState(null);
-	const [pageNumber, setPageNumber] = useState(1);
-
-	function onDocumentLoadSuccess({ numPages }) {
-		setNumPages(numPages);
-	}
-
 	return (
 		<div className="Page" id="cv">
 			<span className="Content">
 				<h2>CV</h2>
-				<div className="CV-Container">
-					<Document 
-						file={cvDriveURL}
-						onLoadSuccess={onDocumentLoadSuccess}
-						onLoadError={(error) => {
-							console.error('Document load error:', error)
-							toast.error('Failed to load CV: ' + error.message, 
-								{
-									position: 'bottom-center',
-									hideProgressBar: true,
-									closeOnClick: true,
-									progress: undefined,
-								})
-							}
-						}
-					>
-						<Page pageNumber={pageNumber} />
-					</Document>
+				<div className="CV">
 				</div>
-				{/* Page number and next and previous buttons */}
-				<p>Page {pageNumber} of {numPages}</p>
-				<button className="CV-Button" id="previous"
-					disabled={pageNumber <= 1}
-					onClick={() => setPageNumber(pageNumber - 1)}
-				>Previous</button>
-				<button className="CV-Button" id="next"
-					disabled={pageNumber >= numPages}
-					onClick={() => setPageNumber(pageNumber + 1)}
-				>Next</button>
-				<br />
 				{/* Download button */}
 				<button onClick={handleDownload}>
 					Download as PDF
